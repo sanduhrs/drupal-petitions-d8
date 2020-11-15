deda27ea50a75e643b1df8df461adc29d9b3eedd
Stefan Auditor <stefan@auditor.email>
2020-11-15 12:11:08 +0100

    Remove query range limit from results block

--- web/modules/contrib/spn/src/Plugin/Block/PetitionResultsBlock.php
+++ web/modules/contrib/spn/src/Plugin/Block/PetitionResultsBlock.php
@@ -19,27 +19,27 @@ class PetitionResultsBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    
+
     // variables
     $route = \Drupal::routeMatch()->getRouteName();
-    
+
     // if route is a node
     if ($route == 'entity.node.canonical') {
-        
+
       // variables
       $signatures = [];
       $nid = \Drupal::routeMatch()->getParameter('node')->Id();
       $bundle = \Drupal::routeMatch()->getParameter('node')->getType();
-      
+
       // if node is a petition then load the results
       if ($bundle == 'petition') {
-        
+
         // establish a basic connection with all petitions
         $connection = \Drupal\Core\Database\Database::getConnection();
         $query = $connection->select('petition_signatures', 'petition');
         $query->condition('petition.nid', $nid);
         $query->condition('petition.validated', 1);
-        
+
         // get the number of results and exit if empty
         $petition_count = clone $query;
         $query_result_count = $petition_count->countQuery()->execute()->fetchField();
@@ -47,7 +47,7 @@ class PetitionResultsBlock extends BlockBase {
         if ($query_result_count == 0) {
           return;
         }
-        
+
         // fetch all non-anonymous applications
         $query->condition('petition.anonymous_opinion', 0);
         $query->addField('petition', 'is_drupal_user');
@@ -55,16 +55,16 @@ class PetitionResultsBlock extends BlockBase {
         $query->addField('petition', 'uid', 'id_external');
         $query->addField('petition', 'comment');
         $query->orderBy('petition.timestamp', 'DESC');
-        $query->range(0, 20);
+        //$query->range(0, 20);
         $query_result = $query->execute()->fetchAll();
-        
+
         // make a list of all signatures
         if (!empty($query_result)) {
           foreach ($query_result as $user_key => $user_data) {
-            
+
             // if the user is a drupal user get his data
             if($user_data->{'is_drupal_user'} == 1) {
-            
+
               // get the user info
               $query_user = $connection->select('users_field_data', 'user');
               $query_user->condition('user.uid', $user_data->{'id_drupal'});
@@ -75,7 +75,7 @@ class PetitionResultsBlock extends BlockBase {
               //$query_user->addField('user__field_nom', 'field_nom_value', 'lastname');
               $query_user->addField('user', 'mail');
               $user_result = $query_user->execute()->fetchObject();
-              
+
               // add signature
               $signatures[] = [
                 'name' => $user_result->{'name'},
@@ -83,10 +83,10 @@ class PetitionResultsBlock extends BlockBase {
                 'mail' => $user_result->{'mail'},
                 'comment' => $user_data->{'comment'},
               ];
-              
+
             // if the user is not signed up get his data from our custom sql database
             } else {
-              
+
               // get the user info
               $query_user = $connection->select('petition_user', 'user');
               $query_user->condition('user.user_id', $user_data->{'id_external'});
@@ -94,7 +94,7 @@ class PetitionResultsBlock extends BlockBase {
               $query_user->addField('user', 'surname', 'lastname');
               $query_user->addField('user', 'email', 'mail');
               $user_result = $query_user->execute()->fetchObject();
-              
+
               // add signature
               $signatures[] = [
                 'name' => $user_result->{'name'},
@@ -102,12 +102,12 @@ class PetitionResultsBlock extends BlockBase {
                 'mail' => $user_result->{'mail'},
                 'comment' => $user_data->{'comment'},
               ];
-              
+
             }
-            
-          } 
+
+          }
         }
-        
+
         return [
           '#machine_name'     => $this->getConfiguration()['id'],
           '#signatures'       => $signatures,
@@ -117,9 +117,9 @@ class PetitionResultsBlock extends BlockBase {
             'max-age' => 0,
           ],
         ];
-        
+
       }
-      
+
     }
   }
 
@@ -129,5 +129,5 @@ class PetitionResultsBlock extends BlockBase {
   public function getCacheMaxAge() {
       return 0;
   }
-  
+
 }
