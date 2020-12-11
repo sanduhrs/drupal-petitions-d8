cd12b2a435d753b8534a79232d22ce267e1edcfc
sanduhrs <stefan@auditor.email>
2020-12-11 16:12:51 +0100

    Add saldo block

---
+++ web/modules/custom/stripe_campaign/src/Plugin/Block/SaldoBlock.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\stripe_campaign\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a saldo block.
+ *
+ * @Block(
+ *   id = "stripe_campaign_saldo",
+ *   admin_label = @Translation("Saldo"),
+ *   category = @Translation("Campaign")
+ * )
+ */
+class SaldoBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $connection;
+
+  /**
+   * Constructs a new SaldoBlock instance.
+   *
+   * @param array $configuration
+   *   The plugin configuration, i.e. an array with configuration values keyed
+   *   by configuration option name. The special key 'context' may be used to
+   *   initialize the defined contexts by setting it to an array of context
+   *   values keyed by context names.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Database\Connection $connection
+   *   The database connection.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Connection $connection) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->connection = $connection;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('database')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $result = $this->connection->query('select SUM(amount) as amount from campaign WHERE nid = :nid', [':nid' => $node->id()])->fetchObject();
+    $build['content'] = [
+      '#markup' => $this->t('@sum€ von @target€', ['@sum' => $result->amount, '@target' => '40000']),
+    ];
+    return $build;
+  }
+
+}
